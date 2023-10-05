bfef0c3d0eaecc2380626c3885c558ecee0f7a20
byron korir <byron.korir@student.moringaschool.com>
2023-10-05 10:11:55 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name:"",
+    streetAddress:""
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a new object with the spread operator
+    const updatedEmployee = { ...employee };
+  
+    // Update the value for the specified key
+    updatedEmployee[key] = value;
+  
+    // Return the updated object
+    return updatedEmployee;
+  }
+  
+ //destructively update employees
+ function  destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+ }
+
+ //deleting non destructively
+ function deleteFromEmployeeByKey(employee, key){
+    const deleteEmployee1 ={...employee}
+    //deleting the new object
+    delete deleteEmployee1[key]
+    return deleteEmployee1
+
+ }
+
+ //destructively deleting an object
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return  employee
+ }
